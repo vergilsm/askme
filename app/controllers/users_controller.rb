000577efeba38890ed7e858605b58c0f266b264a
vergil <vergil.sm@yandex.ru>
2017-06-13 12:22:02 +0300

    changed users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет мир"
+    @user = User.new(
+      name: 'Dima',
+      username: 'vergilsm',
+      avatar_url: 'https://avatarko.ru/img/avatar/1/computer_buben.jpg'
+    )
   end
 end
