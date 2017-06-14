4624c5de8640564917e3453d68fc0a8180a00aa5
vergil <vergil.sm@yandex.ru>
2017-06-14 14:35:11 +0300

    added partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,10 @@ class UsersController < ApplicationController
       username: 'vergilsm',
       avatar_url: 'https://avatarko.ru/img/avatar/1/computer_buben.jpg'
     )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('14.06.2017')),
+      Question.new(text: 'Как прошел день?', created_at: Date.parse('14.06.2017')),
+    ]
   end
 end
