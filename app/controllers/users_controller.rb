afba9306e7d2658de6011a603359fbf52eba85ce
vergil <vergil.sm@yandex.ru>
2017-06-14 17:16:53 +0300

    added and changed styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Dima',
+        username: 'vergilsm',
+        avatar_url: 'https://avatarko.ru/img/avatar/1/computer_buben.jpg'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
   end
 
   def new
@@ -19,5 +32,7 @@ class UsersController < ApplicationController
       Question.new(text: 'Как дела?', created_at: Date.parse('14.06.2017')),
       Question.new(text: 'Как прошел день?', created_at: Date.parse('14.06.2017')),
     ]
+
+    @new_question = Question.new
   end
 end
