e1d7e5283c3d23f1f6c49e6783e6af12696d2fe0
vergil <vergil.sm@yandex.ru>
2017-06-22 17:14:05 +0300

    added field authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,28 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Dima',
-        username: 'vergilsm',
-        avatar_url: 'https://avatarko.ru/img/avatar/1/computer_buben.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Misha',
-        username: 'aristofun'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+  end
+
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Dima',
-      username: 'vergilsm',
-      avatar_url: 'https://avatarko.ru/img/avatar/1/computer_buben.jpg'
-    )
+  end
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('14.06.2017')),
-      Question.new(text: 'Как прошел день?', created_at: Date.parse('14.06.2017')),
-    ]
-
-    @new_question = Question.new
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
