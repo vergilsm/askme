700a627d08b8fe026440488f37f36a0e110938c8
vergil <vergil.sm@yandex.ru>
2017-07-04 14:19:25 +0300

    changed path at registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
    if  @user.save
-     redirect_to root_url, notice: 'Пользователь зарегестрирован!'
+     redirect_to user_path(@user), notice: 'Пользователь зарегестрирован!'
    else
      render 'new'
    end
