54f8dc90733ab6d6a3087a8f0c431f2c744a2adb
vergil <vergil.sm@yandex.ru>
2017-08-07 17:23:48 +0300

    question_author

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+  belongs_to :author, class_name: "User"
 
   validates :text, length: {maximum: 255}
   validates :text, :user, presence: true
---
+++ db/migrate/20170807130018_add_author_to_question.rb
@@ -0,0 +1,5 @@
+class AddAuthorToQuestion < ActiveRecord::Migration
+  def change
+    add_column :questions, :author_id, :integer
+  end
+end
